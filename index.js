dd28d4ba73a4d414a723d37f2edbd0419cde68f5
Bijit-Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2020-10-25 11:22:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ io.on('connection', (socket) => {
     console.log('message: ' + msg);
   });
 });
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log('listening on :3000');
 });
